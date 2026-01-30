cae7d7cfc6769ac02df21b9a3a887a8e4a71b103
JiQuMao <66835939+JiQuMao@users.noreply.github.com>
2026-01-31 01:00:04 +0800

    Update script.sh

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,6 +1,14 @@
 # 移除luci-app-attendedsysupgrade软件包
 sed -i "/attendedsysupgrade/d" $(find ./feeds/luci/collections/ -type f -name "Makefile")
 
+# 移除要替换的包
+rm -rf feeds/luci/applications/luci-app-argon-config
+rm -rf feeds/luci/themes/luci-theme-argon
+
+# 替换包
+git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon feeds/luci/themes/luci-theme-argon
+git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config feeds/luci/applications/luci-app-argon-config
+
 # Git稀疏克隆，只克隆指定目录到本地
 function git_sparse_clone() {
   branch="$1" repourl="$2" && shift 2
